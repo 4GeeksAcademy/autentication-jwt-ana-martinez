cbc1110f798e8a309162403443efe8d0b381d48b
AnaJaneMM <94802408+AnaJaneMM@users.noreply.github.com>
2025-11-23 16:12:24 +0000

    escribí singup xD

--- src/api/routes.py
+++ src/api/routes.py
@@ -23,7 +23,7 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/singup', methods=['POST'])
+@api.route('/signup', methods=['POST'])
 def signup():
     body = request.get_json()
 
